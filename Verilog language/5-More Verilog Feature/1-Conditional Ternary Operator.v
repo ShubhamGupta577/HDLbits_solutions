c89535f785188bdea340758dc79b9a562c67bbd3
Shubham Gupta <57499043+ShubhamGupta577@users.noreply.github.com>
2022-07-04 22:23:07 +0530

    Conditional Ternary Operator

---
+++ Verilog language/5-More Verilog Feature/1-Conditional Ternary Operator.v
@@ -0,0 +1,9 @@
+module top_module (
+    input [7:0] a, b, c, d,
+    output [7:0] min);
+    wire [7:0] x,y,z;
+    assign x=a<b?a:b;
+    assign y=x<c?x:c;
+    assign min=y<d?y:d;
+    
+endmodule
